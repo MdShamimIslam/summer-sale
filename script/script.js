0497491fb6cb8655ea91c7937338bcc397b9403e
MdShamimIslam <shamim401897@gmail.com>
2023-10-04 12:49:00 +0600

    dynamic name part added

--- script/script.js
+++ script/script.js
@@ -9,16 +9,33 @@ function getElementId(textId) {
 function buyNowBtn() {
   const accessories1 = getElementId("accessories1");
   
-  addToCalEntry("K-Accessories")
+  addToCalEntry("K-Accessories",accessories1)
 }
 
 function buyNowBtn2() {
   const accessories2 = getElementId("accessories2");
+  addToCalEntry("K-Accessories",accessories2)
 
 }
 
 function buyNowBtn3() {
   const homeMaker = getElementId("home-maker");
+  addToCalEntry("Home Cooker",homeMaker)
+  
+}
+function buyNowBtn4() {
+  const capPrice = getElementId("cap-price");
+  addToCalEntry("Sports Back Cap",capPrice)
+  
+}
+function buyNowBtn5() {
+  const jerseyPrice = getElementId("jersey-price");
+  addToCalEntry("Full Jersey Set",jerseyPrice)
+  
+}
+function buyNowBtn6() {
+  const catesPrice = getElementId("cates-price");
+  addToCalEntry("Sports cates",catesPrice)
   
 }
 
@@ -28,12 +45,7 @@ function addToCalEntry (typeOfName){
         const p = document.createElement('p');
         p.innerHTML = `${count+1}. ${typeOfName}`;
         dynamicName.appendChild(p);  
-
 }
-
-
-
-
 
 
 
