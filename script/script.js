64787dcba08f49b14559b37668230046675258c5
MdShamimIslam <shamim401897@gmail.com>
2023-10-04 12:35:54 +0600

    get accessories value

--- script/script.js
+++ script/script.js
@@ -1,1 +1,47 @@
-console.log('hi');
+// get innerText by Id parameter
+function getElementId(textId) {
+  const element = document.getElementById(textId);
+  const elementValueString = element.innerText;
+  const elementValue = parseFloat(elementValueString);
+  return elementValue;
+}
+
+function buyNowBtn() {
+  const accessories1 = getElementId("accessories1");
+  
+  addToCalEntry("K-Accessories")
+}
+
+function buyNowBtn2() {
+  const accessories2 = getElementId("accessories2");
+
+}
+
+function buyNowBtn3() {
+  const homeMaker = getElementId("home-maker");
+  
+}
+
+function addToCalEntry (typeOfName){
+        const dynamicName = document.getElementById('dynamic-name');
+        const count = dynamicName.childElementCount;
+        const p = document.createElement('p');
+        p.innerHTML = `${count+1}. ${typeOfName}`;
+        dynamicName.appendChild(p);  
+
+}
+
+
+
+
+
+
+
+// const valueSpan = document.getElementById('value');
+// const addButton = document.getElementById('addButton');
+// let counter = 0;
+// addButton.addEventListener('click', () => {
+//     counter++;
+//     valueSpan.textContent = counter;
+// });
+
